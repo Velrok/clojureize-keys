fb8b88474151407755ec9fd6ffecc9deab12b376
Waldemar <waldemar.schwan@uswitch.com>
2020-12-18 23:51:15 +0000

    create safe keyword fn
    which returns strings instead of invalid keywords

--- src/clojureize_keys/core.clj
+++ src/clojureize_keys/core.clj
@@ -4,6 +4,26 @@
     [clojure.string :as string]
     [clojure.walk :refer [postwalk]]))
 
+(defn- invalid-as-keyword?
+  [s]
+  (string/includes? s " "))
+
+(defn keyword-or-str
+  "Returns a keyword iff it would be valid, string otherwise."
+  ([s]
+   (if (invalid-as-keyword? s)
+     s
+     (keyword s)))
+  ([ns s]
+   (if (invalid-as-keyword? s)
+     (str ns "/" s)
+     (keyword ns s))))
+(s/fdef keyword-or-str
+        :args (s/or :just-str (s/cat :s string?)
+                    :ns-and-str (s/cat :ns string? :s string?))
+        :ret (s/or :keyword keyword? :string string?))
+
+
 (defmulti key-fn type)
 
 ;; (defmethod key-fn java.lang.String [k] k)
@@ -12,8 +32,8 @@
   [k]
   (let [transform #(string/replace % #"_" "-")]
     (if-let [ns (namespace k)]
-      (keyword ns (transform (name k)))
-      (keyword (transform (name k))))))
+      (keyword-or-str ns (transform (name k)))
+      (keyword-or-str (transform (name k))))))
 
 (defmethod key-fn :default [k] k) ;; no transform by default
 
