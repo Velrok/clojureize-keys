cd71d9b5dc313e5404578859966762cf66ec1a5a
Waldemar <waldemar.schwan@uswitch.com>
2020-12-18 23:51:42 +0000

    property testing and performance baseline

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.0"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.0"]
+                                  [criterium "0.4.6"]]}}
   :repl-options {:init-ns clojureize-keys.core})
--- test/clojureize_keys/core_test.clj
+++ test/clojureize_keys/core_test.clj
@@ -1,20 +1,58 @@
 (ns clojureize-keys.core-test
   (:require
-    ;[clojure.spec.test :as stest]
-    ; [clojure.spec.gen :as gen]
-    ; [clojure.spec :as s]
+    [criterium.core :as criterium]
+    [clojure.spec.gen.alpha :as gen]
+    [clojure.spec.test.alpha :as stest]
     [clojure.test :refer [testing deftest is]]
-    [clojureize-keys.core :refer [clojureize-keys]]
+    [clojureize-keys.core :refer [clojureize-keys keyword-or-str]]
     ))
+
+(def rand-str-seq (repeatedly #(gen/generate (gen/gen-for-pred string?))))
+(def rand-map-seq (repeatedly #(gen/generate (gen/gen-for-pred map?))))
+
+(deftest keyword-or-str-test
+  (testing "examples"
+    (is (= :a-b (keyword-or-str "a-b")))
+    (is (= "a b" (keyword-or-str "a b")))
+    (is (= :a/b (keyword-or-str "a/b")))
+    (is (= :a.x/b (keyword-or-str "a.x/b")))
+    )
+  (testing "properties"
+    (testing "will not throw exceptions"
+      (stest/summarize-results (stest/check  `keyword-or-str))))
+  (testing "performance"
+    (let [sample-count 1000
+          avg-exec-target-ns 1500
+          _pregen (doall (take sample-count rand-str-seq))
+          [duration-ns _] (criterium/time-body (doseq [s (take sample-count rand-str-seq)]
+                                                 (keyword-or-str s))) ]
+      (is (<= (/ duration-ns sample-count) 
+             avg-exec-target-ns)))))
 
 ;; TODO:
 ;; - use  prob based testing to run though different variants of keywords and strings are keys
 ;; - do some basic performance testing -> use snake kebab as base
 
 (deftest clojureize-keys-test
-  (testing "basics"
+  (testing "examples"
     (is (= {:a-b :x} (clojureize-keys {:a_b :x})))
     (is (= {:a-b :x} (clojureize-keys {:aB :x})))
     (is (= {:x.y.z/a-b :x} (clojureize-keys {:x.y.z/a_b :x}))))
-  ;(stest/check-var #'clojureize-keys :num-tests 100)
+  (testing "properties"
+    (testing "doesnt throw"
+      (stest/summarize-results (stest/check  `clojureize-keys))))
+  (testing "performance"
+    (let [sample-count 1000
+          avg-exec-target-ns 42000
+          _pregen (doall (take sample-count rand-map-seq))
+          [duration-ns _] (criterium/time-body (doseq [m (take sample-count rand-map-seq)]
+                                                 (clojureize-keys m))) ]
+      (is (<= (float (/ duration-ns sample-count)) 
+             avg-exec-target-ns)))))
+
+(comment
+  (keyword-or-str-test)
+  (clojureize-keys-test)
+  (gen/generate (gen/gen-for-pred map?))
   )
+
